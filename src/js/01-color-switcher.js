e1a805e1ec4c0eecb72737c72983f4b840912cac
Oleksandr Pikush <pikush.oleksandr@gmail.com>
2022-12-04 22:34:13 +0200

    +task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+stopBtn.disabled = true;
+let timerId = null;
 
+startBtn.addEventListener("click", () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  changeBackgroundColor();
+  timerId = setInterval(changeBackgroundColor, 1000);
+});
+btnStop.addEventListener("click", (onClick) => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function changeBackgroundColor() {
+  document.body.style.background = getRandomHexColor();
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,23 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from "notiflix";
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
